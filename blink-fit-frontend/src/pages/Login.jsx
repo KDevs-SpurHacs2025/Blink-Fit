cb2b633eb260fb5f1440c896c42d10b31ee4daf2
unknown <jaym991776@gmail.com>
2025-06-21 19:41:53 -0400

    Error Fixed: Solve the error in Survey, questions and Modal

--- blink-fit-frontend/src/pages/Login.jsx
+++ blink-fit-frontend/src/pages/Login.jsx
@@ -32,12 +32,12 @@ export default function Login() {
         <PrimaryButton onClick={() => navigate("/loading")}>
           Continue with Google
         </PrimaryButton>
-<button
-  onClick={handleGoogleLogin}
-  className="bg-green-500 w-full text-black py-2 px-4 rounded-lg hover:bg-opacity-90 w-full mb-4 transition"
->
-  Enter as guest
-</button>
+        <button
+          onClick={handleGoogleLogin}
+          className="bg-green-500 w-full text-black py-2 px-4 rounded-lg hover:bg-opacity-90 w-full mb-4 transition"
+        >
+          Enter as guest
+        </button>
         {/* <button
           onClick={handleStartEyeTracking}
           className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 w-full mt-4 transition"
